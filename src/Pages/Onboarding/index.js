8fda1382645354a9126737a5c9503698bde6b689
Sam Abiodun <samuelabiodun324@gmail.com>
2023-06-10 14:54:32 +0100

    signup-update

--- src/Pages/Onboarding/index.js
+++ src/Pages/Onboarding/index.js
@@ -13,14 +13,17 @@ const Onboarding = () => {
             of finding reliable cleaners, which is why we're dedicated to
             setting a new standard in the industry. Our expert cleaners are
             highly trained, experienced, and pride themselves on punctuality,
-            courtesy, and respect for your space,
+            courtesy, and respect for your space.
           </p>
 
           <div className="btn">
             <Link to="/login">
             <button>Login</button>
             </Link>
+
+            <Link to="/signup">
             <button>SignUp</button>
+            </Link>
           </div>
           
         </div>
--- src/Pages/SignUp/index.js
+++ src/Pages/SignUp/index.js
@@ -1,12 +1,138 @@
+import { Form, Formik, Field,  } from "formik";
+import * as yup from "yup";
+const SignUp = () => {
+  const initialValue = {
+    userName: "",
+    email: "",
+    password: "",
+    contactNumber: "",
+    gender: ""
+  };
 
-import React from 'react'
+  const signUpSchema = yup.object().shape({
+    email: yup.string().email("Invalid email format").required("Required*"),
 
-function SignUp() {
+    userName: yup.string().required('Required*'),
+
+    contactNumber: yup
+    .string()
+    .matches(/^(\+234|0)\d{10}$/, 'Invalid contact number')
+    .required('Required*'),
+    gender: yup.string().oneOf(['MALE', 'FEMALE'], 'Invalid gender').required('Required*'),
+    password: yup
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .max(30, "Password must be at least 30 characters")
+      .required("Required*"),
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  }
+
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="signUp">
+      <div className="design2">
+      <div className="form2">
+      <Formik
+            onSubmit={onSubmit}
+            validationSchema={signUpSchema}
+            initialValues={initialValue}
+          >
 
-export default SignUp
+    {({ isSubmitting, isValid }) => (
+            <Form>
+                  <Field name="email">
+                  {({ field, meta }) => (
+                    <div>
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        className="inputFieldA"
+                        {...field}
+                      />
+                      {meta.touched && meta.error ? (
+                        <p className="text">{meta.error}</p>
+                      ) : null}
+                    </div>
+                    )}
+                    </Field>
+
+                    <Field name="password">
+                  {({ field, meta }) => (
+                    <div>
+                      <input
+                        type="password"
+                        placeholder="Password"
+                        className="inputFieldB"
+                        {...field}
+                      />
+                      {meta.touched && meta.error ? (
+                        <p className="text">{meta.error}</p>
+                      ) : null}
+                    </div>
+                  )}
+                </Field>
+
+                <Field name="userName">
+                  {({ field, meta }) => (
+                    <div>
+                      <input
+                        type="userName"
+                        placeholder="Username"
+                        className="inputFieldC"
+                        {...field}
+                      />
+                      {meta.touched && meta.error ? (
+                        <p className="text">{meta.error}</p>
+                      ) : null}
+                    </div>
+                  )}
+                </Field>
+
+                <Field name="contactNumber">
+                  {({ field, meta }) => (
+                    <div>
+                      <input
+                        type="contactNumber"
+                        placeholder="Contact number"
+                        className="inputFieldD"
+                        {...field}
+                      />
+                      {meta.touched && meta.error ? (
+                        <p className="text">{meta.error}</p>
+                      ) : null}
+                    </div>
+                  )}
+                </Field>
+
+                <Field name="gender">
+                  {({ field, meta }) => (
+                    <div>
+                      <input
+                        type="gender"
+                        placeholder="Gender"
+                        className="inputFieldE"
+                        {...field}
+                      />
+                      {meta.touched && meta.error ? (
+                        <p className="text">{meta.error}</p>
+                      ) : null}
+                    </div>
+                  )}
+                </Field>
+                <div>
+                  <button className="bt2" type="submit" aria-label="">
+                    Sign up
+                  </button>
+                </div>
+              </Form>
+              )}
+              </Formik>
+              </div>
+            </div>
+          </div>
+        );
+      };
+                
+  export default SignUp;
